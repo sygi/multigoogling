286933be9f68d9fd848308d461f515a688a5c215
Jakub Sygnowski <sygnowski@gmail.com>
2013-03-29 17:16:26 +0100

    dziala?

--- exec.js
+++ exec.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 $("#submit").click(function(){
-	$("#texta").val('sygi');
+	var queries = $("#texta").val();
+	var list = queries.split('\n');
+	for(var i = 0; i < list.length; i++){
+		chrome.tabs.create({url: "http://www.google.com/search?q=" + list[i]});
+	}
 });
-
 });
